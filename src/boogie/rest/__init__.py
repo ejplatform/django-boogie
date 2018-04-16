2d5058993a3023fac651f9a692e546d994a775f1
fabiomacedomendes <fabiomacedomendes@gmail.com>
2018-04-16 10:22:57 -0300

    check presence of DRF

--- src/boogie/rest/__init__.py
+++ src/boogie/rest/__init__.py
@@ -1,3 +1,14 @@
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    import rest_framework as _rest
+except ImportError:
+    raise ImproperlyConfigured(
+        'boogie.rest requires Django Rest Framework and some additional '
+        'packages to be installed. Please install it using pip install '
+        'django-boogie[rest].'
+    )
+
 from .base import RestAPI
 
 rest_api = RestAPI()
