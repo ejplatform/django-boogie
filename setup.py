3b14b8d61aa0803f4726e203175d85825a33cd0c
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2018-09-27 11:39:30 -0300

    Update setup.py to read the version number

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-import sys;
-
+import re
+import os
 from setuptools import setup, find_packages
 
-sys.path.append('src')
+init = open(os.path.join('src', 'boogie', '__init__.py')).read()
+version = re.search(r"__version__ = '(\d+\.\d+.\d+)'", init).group(1)
 
 setup(
     name='django-boogie',
+    version=version,
     package_dir={'': 'src'},
     packages=find_packages('src'),
     setup_requires='setuptools >= 30.3',
